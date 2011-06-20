ec315d276239b91454f3e8f3173afad61540ce93
Alberto Monteiro <betimbrasil@gmail.com>
2011-06-20 19:50:31 -0300

    pesquisas com ajax"

--- script.js
+++ script.js
@@ -2,9 +2,72 @@ if (window.location.pathname.indexOf("browse.php") != -1) {
     ajaxTorrentsPage();
 }
 
+if (window.location.pathname.indexOf("pesquisa") != -1) {
+    ajaxTorrentsSearchPage();
+    ajaxTorrentsFormPage();
+}
+
+$("#menu form").submit(function () {
+    ajaxBegin($("#content"));
+    ajaxStop($("#content"));
+    $.ajax({
+        url: $(this).attr("action"),
+        data: $(this).serialize(),
+        success: function (data, status) {
+            $("#content").html($(data).find("#content").html());
+            ajaxTorrentsSearchPage();
+        },
+        complete: function (data, status) {
+            window.history.pushState(document, document.title, this.url);
+        }
+    });
+    return false;
+});
+
+function ajaxTorrentsFormPage() {
+    $("#content form").submit(function () {
+        var data = $(this).serialize();
+        var link = $(this).attr("action");
+        var tableIsNull = $("#content table")[1] == null;
+        var tabela = $($("#content table")[1]);
+        if (tableIsNull) {
+            ajaxBegin($("#content"));
+            ajaxStop($("#content"));
+        }
+        else {
+            ajaxBegin(tabela);
+            ajaxStop(tabela);
+        }
+        window.history.pushState(document, document.title, link);
+        $.ajax({
+            url: link,
+            data: data,
+            success: function (data) {
+                var html = $($(data).find("#content table")[1]).clone()
+                var pages = $($(data).find("#content table")[1]).next().clone()
+                var pages1 = $($(data).find("#content table")[1]).prev().clone()
+                if (tableIsNull) {
+                    $("#content").append(pages1);
+                    $("#content").append(html);
+                    $("#content").append(pages);
+                } else {
+                    tabela.html(html);
+                    tabela.next().html(pages);
+                    tabela.prev().html(pages1);
+                }
+                ajaxTorrentsSearchPage();
+            }
+        });
+        return false;
+    });
+}
+
 function ajaxTorrentsPage() {
+    ajaxBegin($("#tabela1"));
+    ajaxStop($("#tabela1"));
     $("#tabela1").next().find("a").click(function () {
         var link = $(this).attr("href");
+        window.history.pushState(document, document.title, link);
         $.ajax({
             url: link,
             success: function (data) {
@@ -14,5 +77,78 @@ function ajaxTorrentsPage() {
             }
         });
         return false;
+    });
+}
+
+function ajaxTorrentsSearchPage() {
+    var tabela = $($("#content table")[1]);
+    ajaxBegin(tabela);
+    ajaxStop(tabela);
+    tabela.next().find("a").click(function () {
+        var link = $(this).attr("href");
+        window.history.pushState(document, document.title, link);
+        $.ajax({
+            url: link,
+            success: function (data) {
+                var html = $($(data).find("#content table")[1]).html()
+                var pages = $($(data).find("#content table")[1]).next().html()
+                var pages1 = $($(data).find("#content table")[1]).prev().html()
+                tabela.html(html);
+                tabela.next().html(pages);
+                tabela.prev().html(pages1);
+                ajaxTorrentsSearchPage();
+            }
+        });
+        return false;
+    });
+    tabela.prev().find("a").click(function () {
+        var link = $(this).attr("href");
+        window.history.pushState(document, document.title, link);
+        $.ajax({
+            url: link,
+            success: function (data) {
+                var html = $($(data).find("#content table")[1]).html()
+                var pages = $($(data).find("#content table")[1]).next().html()
+                var pages1 = $($(data).find("#content table")[1]).prev().html()
+                tabela.html(html);
+                tabela.next().html(pages);
+                tabela.prev().html(pages1);
+                ajaxTorrentsSearchPage();
+            }
+        });
+        return false;
+    });
+    return false;
+}
+
+function ajaxBegin(element) {
+    element.ajaxStart(function () {
+        var top = element.position().top;
+        var left = element.position().left;
+        var altura = element.height();
+        var largura = element.width();
+        $("body").append("<div id='loadingDivAjax' style='position:absolute'></div>");
+        $("body").append("<div id='loadingDivAjaxText'><center><h1>Carregando</h1><br /><img src='http://www.nestle.pt/cozinhar/images/ajax_loading_bar.gif' /></center></div>");
+        $("#loadingDivAjax").css({
+            top: top + "px",
+            left: left + "px",
+            width: largura + "px",
+            height: altura + "px",
+            'background-image': 'url("http://ajax.aspnetcdn.com/ajax/jquery.ui/1.8.10/themes/ui-lightness/images/ui-bg_diagonals-thick_20_666666_40x40.png")',
+            opacity: 0.5
+        });
+        $("#loadingDivAjaxText").css({
+            position: "absolute",
+            top: top + "px",
+            left: left + "px",
+            width: largura + "px"
+        });
+    });
+}
+
+function ajaxStop(element) {
+    element.ajaxStop(function () {
+        $("#loadingDivAjax").remove();
+        $("#loadingDivAjaxText").remove();
     });
 }
